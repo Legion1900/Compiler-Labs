b355e1fc9eb0a51c8fa4936951d7ec2f33447189
Chumachenko Roman <rom4ik730@gmail.com>
2019-12-14 14:02:51 +0200

    In-depth scope search implemented.

---
+++ src/main/java/optimization/DeepScopeOptimizer.kt
@@ -0,0 +1,32 @@
+package optimization
+
+import java.util.*
+
+class DeepScopeOptimizer {
+    fun optimize(exp: List<String>): List<String> {
+        val scopes = findScopes(exp)
+        for (nesting in scopes.keys) {
+            scopes.getValue(nesting).forEach { print("$it ") }
+            println()
+        }
+        TODO("Finish me")
+    }
+
+    private fun findScopes(exp: List<String>): Map<Int, List<Pair<Int, Int>>> {
+        val scopes = mutableMapOf<Int, MutableList<Pair<Int, Int>>>()
+        val opened = Stack<Int>()
+        for (i in exp.indices) {
+            if (exp[i] == "(")
+                opened += i
+            if (exp[i] == ")") {
+                scopes[opened.size]?.run {
+                    add(opened.pop() to i)
+                } ?: run {
+                    val list = mutableListOf(opened.pop() to i)
+                    scopes[opened.size] = list
+                }
+            }
+        }
+        return scopes
+    }
+}
